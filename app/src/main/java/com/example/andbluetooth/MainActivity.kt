5645a7a422200ed006b89a5ad42e8c0cbe3ceee9
dcbaek <ehdco8988@gmail.com>
2022-06-27 17:30:09 +0900

    Permission for Bluetooth Device Scan

--- app/src/main/java/com/example/andbluetooth/MainActivity.kt
+++ app/src/main/java/com/example/andbluetooth/MainActivity.kt
@@ -1,35 +1,97 @@
 package com.example.andbluetooth
 
+import android.Manifest
 import android.bluetooth.BluetoothAdapter
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import android.widget.ToggleButton
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.view.isVisible
+import java.security.Permissions
 
 class MainActivity : AppCompatActivity() {
+
     private val REQUEST_ENABLE_BT=1
     private var bluetoothAdapter: BluetoothAdapter? = null
+
+    private val REQUEST_PERMISSIONS = 2
+    private val PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
+    private fun hasPermissions(context: Context?, permissions: Array<String>): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (permission in permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    //Permission 확인
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            REQUEST_PERMISSIONS -> {
+                //if request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission must be granted", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val bleOnOffBtn:ToggleButton = findViewById(R.id.ble_on_off_btn)
+        val scanBtn: Button = findViewById(R.id.scanBtn)
+
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 
         if(bluetoothAdapter!=null){
             // Device doesn't support Bluetooth
             if(bluetoothAdapter?.isEnabled==false){
                 bleOnOffBtn.isChecked = true
+                scanBtn.isVisible = false
             } else{
                 bleOnOffBtn.isChecked = false
+                scanBtn.isVisible =true
             }
         }
 
-        bleOnOffBtn.setOnCheckedChangeListener { _, isChecked ->
-            bluetoothOnOff()
+        bleOnOffBtn.setOnCheckedChangeListener {
+                _,
+                isChecked -> bluetoothOnOff()
+                scanBtn.visibility = if(scanBtn.visibility == View.VISIBLE) {
+                    View.INVISIBLE
+                } else {
+                    View.VISIBLE
+                }
         }
 
     }
+
     fun bluetoothOnOff(){
         if (bluetoothAdapter == null) {
             // Device doesn't support Bluetooth
