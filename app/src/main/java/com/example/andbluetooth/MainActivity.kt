94ac69b868e02b5ce95a76aa49b8e1e418e2f85e
dcbaek <ehdco8988@gmail.com>
2022-06-28 10:52:44 +0900

    Bluetooth Device Scan

--- app/src/main/java/com/example/andbluetooth/MainActivity.kt
+++ app/src/main/java/com/example/andbluetooth/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.andbluetooth
 
 import android.Manifest
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -17,16 +21,66 @@ import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.view.isVisible
 import java.security.Permissions
+import java.util.regex.MatchResult
 
 class MainActivity : AppCompatActivity() {
 
-    private val REQUEST_ENABLE_BT=1
+    private val REQUEST_ENABLE_BT = 1
     private var bluetoothAdapter: BluetoothAdapter? = null
 
     private val REQUEST_PERMISSIONS = 2
     private val PERMISSIONS = arrayOf(
         Manifest.permission.ACCESS_FINE_LOCATION
     )
+
+    private var scanning: Boolean = false
+    private var devicesArr = ArrayList<BluetoothDevice>()
+    private val SCAN_PERIOD = 1000
+    private val handler = Handler()
+
+    private val mLeScanCallback = @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    object : ScanCallback() {
+        override fun onScanFailed(errorCode: Int) {
+            super.onScanFailed(errorCode)
+            Log.d("scanCallback", "BLE Scan Failed : " + errorCode)
+        }
+
+        override fun onBatchScanResults(results: MutableList<ScanResult>?) {
+            super.onBatchScanResults(results)
+            results?.let {
+                //results is not null
+                for (result in it) {
+                    if (!devicesArr.contains(result.device) && result.device.name != null) devicesArr.add(
+                        result.device
+                    )
+                }
+            }
+        }
+
+        override fun onScanResult(callbackType: Int, result: ScanResult?) {
+            super.onScanResult(callbackType, result)
+            result?.let {
+                //result is not null
+                if (!devicesArr.contains(it.device) && it.device.name != null) devicesArr.add(it.device)
+                recyclerViewAdapter.notifyDataSetChanged()
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun scanDevice(state: Boolean) = if (state) {
+        handler.postDelayed({
+            scanning = false
+            bluetoothAdapter?.bluetoothLeScanner?.stopScan(mLeScanCallback)
+        }, SCAN_PERIOD)
+        scanning = true
+        devicesArr.clear()
+        bluetoothAdapter?.bluetoothLeScanner?.startScan(mLeScanCallback)
+    } else {
+        scanning = false
+        bluetoothAdapter?.bluetoothLeScanner?.stopScan(mLeScanCallback)
+    }
+
 
     private fun hasPermissions(context: Context?, permissions: Array<String>): Boolean {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
@@ -90,6 +144,13 @@ class MainActivity : AppCompatActivity() {
                 }
         }
 
+        scanBtn.setOnClickListener{
+            v:View? -> // Scan Button Oncliick
+            if(!hasPermissions(this, PERMISSIONS)) {
+                requestPermissions(PERMISSIONS, REQUEST_ALL_PERMISSION)
+            }
+            scanDevice(true)
+        }
     }
 
     fun bluetoothOnOff(){
